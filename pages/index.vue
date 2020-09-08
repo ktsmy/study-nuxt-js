e9874169daee488fee000f279f966aef9bc48e74
Katsumi Yamashita <yamashita.katsumi@classmethod.jp>
2020-09-08 18:23:46 +0900

    routing
    https://ja.nuxtjs.org/guides/get-started/routing

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,15 @@
 <template>
-  <h1>Hello world!</h1>
+  <main>
+    <h1>Hello world!</h1>
+    <nav>
+      <ul>
+        <li>
+          <nuxt-link to="/fun">Fun page</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/more-fun">More fun page</nuxt-link>
+        </li>
+      </ul>
+    </nav>
+  </main>
 </template>
--- pages/more-fun/index.vue
+++ pages/more-fun/index.vue
@@ -1,3 +1,15 @@
 <template>
-  <h1>Having more fun with Nuxt.js</h1>
+  <main>
+    <h1>Having more fun with Nuxt.js</h1>
+    <nav>
+      <ul>
+        <li>
+          <nuxt-link to="/">Home page</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/fun">Fun page</nuxt-link>
+        </li>
+      </ul>
+    </nav>
+  </main>
 </template>
